95e6a080031c528fab8b9bb59ff1a6fede35d3eb
phattannguyen1234 <helethropetanxion@gmail.com>
2024-01-21 17:32:51 +0700

    lap 1 nodejs, 5.2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fsp = require('fs/promises');
 
 // 5.1
 try{
@@ -8,3 +9,18 @@ try{
     console.error(err);
 }
 // 5.1 **
+
+// 5.2
+async function readandwrite(){
+    try{
+        const index2 = await fsp.readFile('txt/read-this.txt', 'utf-8');
+        console.log("**Below is the content of read-this.txt**", '\n', index2);
+        console.log("**Writing read-this.txt to start.txt**");
+        await fsp.writeFile('txt/start.txt', index2, 'utf-8');
+        console.log("File written successfully");
+    } catch (err){
+        console.error(err);
+    }
+}
+readandwrite();
+// 5.2 **
